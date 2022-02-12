ad39b155d2b48b34feaff81a44ee77c7a327981f
jenelleabrigo <jenelleabrigo@gmail.com>
2022-02-12 18:46:42 +0800

    add files

---
+++ code/template/template/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="/htdocs/data/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&family=Montserrat:wght@100;300;400;500;600;700&family=Noto+Serif:wght@400;700&family=Open+Sans:wght@300;400;500;600;700&family=Roboto:wght@100;300;400;500;700&family=Source+Serif+Pro:wght@300;400;600;700&family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">  </head>
+  <body>
+
+<header>
+  <div class="p-header u-pc-only">
+    helllo dapat lato AKO
+  </div>
+</header>
